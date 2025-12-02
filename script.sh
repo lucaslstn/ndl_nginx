c96d77f8c3dfaef94bb43612abfa23c047c75228
lucaslstn <lucas.lestiennes@gmail.com>
2025-12-02 18:22:38 +0100

    Refactor script for site setup and Nginx config
    Updated the script to include color variables, improved prompts for site creation, and changed the way the Nginx configuration is handled.

--- script.sh
+++ script.sh
@@ -1,25 +1,78 @@
-apt update -y
-apt upgrade -y
-apt autoremove -y
-apt install -y sudo wget curl nginx
-read -p "Nom du site (ex: monsite) : " SITE
-SITE_DIR="/var/www/html/$SITE"
-mkdir -p "$SITE_DIR"
-CONF="/etc/nginx/sites-available/$SITE"
-cat > "$CONF" <<EOF
+#!/bin/bash
+
+# variable pour les couleurs
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
+
+# installation paquets
+echo -e "${GREEN}Mise à jour et installation...${RESET}"
+apt-get update -y
+apt-get install -y git nginx
+
+# prompt infos
+echo ""
+read -p "Nom du dossier du site (ex: mon_site) : " SITE
+read -p "Source ? (1: GitHub, 2: Site vide) : " CHOIX
+
+# Variable pour le chemin du site
+DOSSIER="/var/www/html/$SITE"
+
+# choix
+if [ "$CHOIX" == "1" ]; then
+    # Option GITHUB
+    read -p "Lien HTTPS du repo GitHub : " URL_GIT
+    
+    # On supprime le dossier pour éviter erreur
+    rm -rf "$DOSSIER"
+    
+    echo "Clonage de GitHub..."
+    git clone "$URL_GIT" "$DOSSIER"
+
+
+
+    # à ajoutergestion cron qui met à jour le site automatiquement
+
+else
+    # Option SITE VIDE partir de 0 
+    echo "Création d'un site local..."
+    mkdir -p "$DOSSIER"
+    # création d'un index.html de test
+    echo "<h1>Site Local TEST $SITE</h1>" > "$DOSSIER/index.html"
+fi
+
+# configuration nginx
+
+# gestions droits
+chown -R www-data:www-data "$DOSSIER"
+chmod -R 755 "$DOSSIER"
+
+#création fichier de config nginx
+CONFIG="/etc/nginx/sites-available/$SITE"
+
+# création de la configurations du site NGINX
+cat > "$CONFIG" <<EOF
 server {
     listen 80;
-    server_name _;
-    root $SITE_DIR;
+    root $DOSSIER;
     index index.html;
-
+    server_name _;
     location / {
         try_files \$uri \$uri/ =404;
     }
 }
 EOF
 
-ln -s "$CONF" /etc/nginx/sites-enabled/$SITE
+# activation du site
+ln -sf "$CONFIG" /etc/nginx/sites-enabled/$SITE
+
+# supression du site par défaut de Nginx pour éviter les conflits
 rm -f /etc/nginx/sites-enabled/default
-nginx -t && systemctl reload nginx
-echo "Le site '$SITE' a été créé dans $SITE_DIR et est actif."
+
+# rédemarrage du service NGINX
+echo -e "${GREEN}Redémarrage de Nginx...${RESET}"
+systemctl reload nginx
+
+echo ""
+echo -e "${GREEN}Terminé, le site est prêt.${RESET}"
+echo "Dossier : $DOSSIER"
